21f023fc77c5c0628b70a04cf1da49ef60f55075
Trent_T <trenttuedon2000@gmail.com>
2023-06-02 13:56:34 -0400

    Added Play Again Functionality

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const rock = document.querySelector(".rock"); 
 const paper = document.querySelector(".paper"); 
 const scissors = document.querySelector(".scissors"); 
+const winnerLabel = document.querySelector(".score h3")
+const playAgainLabel = document.querySelector(".score h4")
+let textLabel = ""; 
+let compInput = ""
 
 
 // Render Computer Images 
@@ -32,39 +36,87 @@ function alignLeft(element) {
     element.classList.add("move-img")
 }
 
-function addScore() {
+function addScore(element) {
     const score = document.querySelector(".score"); 
     const game = document.querySelector('.game'); 
     score.style.display = "block";
-    game.insertBefore(score, scissors)
+    game.insertBefore(score, element)
 }
 
 function renderImage(element) {
     const image = document.createElement('img')
     image.src = `${element}.png`
     document.querySelector('.game').appendChild(image); 
+    return image
 }
 
-rock.addEventListener("click", ()=> {
+function evaluateWin(hand1, hand2) {
+    //1 - Rock 
+    //2 - Paper 
+    //3 - Scissors
+
+    console.log(hand1, hand2);
+    if(hand1 === hand2) {
+        return "Its a tie"
+    }
+    else if(hand1 === "paper" && hand2 === "rock") {
+        return "You Win"; 
+    }
+    else if(hand1 === "rock" && hand2 === "scissors") {
+        return "You Win"; 
+    }
+    else if(hand1 === "scissors" && hand2 === "paper") {
+        return "You Win"; 
+    }
+    else {
+        return "You Lose"
+    }
+}
+
+function changeWinnerText(text) {
+    winnerLabel.textContent = text
+    console.log(text)
+}
+
+
+playAgainLabel.addEventListener("click", function handler() {
+    window.location.reload(); 
+})
+
+
+
+rock.addEventListener("click", function handler(){
     alignLeft(rock); 
     deleteElement(paper); 
     deleteElement(scissors); 
-    renderImage(computerInput()); 
-    addScore();
-}); 
+    compInput = computerInput()
+    const image =  renderImage(compInput); 
+    addScore(image);
+    this.removeEventListener("click", handler)
+    textLabel = evaluateWin("rock", compInput); 
+    changeWinnerText(textLabel);
+});    
 
-paper.addEventListener("click", ()=> {
+paper.addEventListener("click", function handler(){
     alignLeft(paper); 
     deleteElement(rock); 
     deleteElement(scissors); 
-    renderImage(computerInput()); 
-    addScore();
+    compInput = computerInput()
+    const image = renderImage(compInput); 
+    addScore(image);
+    this.removeEventListener("click", handler)
+    textLabel = evaluateWin("paper", compInput); 
+    changeWinnerText(textLabel);
 }); 
 
-scissors.addEventListener("click", ()=> {
+scissors.addEventListener("click", function handler(){
     alignLeft(scissors); 
     deleteElement(rock); 
-    deleteElement(paper); 
-    renderImage(computerInput()); 
-    addScore();
+    deleteElement(paper);
+    compInput = computerInput()
+    const image = renderImage(compInput); 
+    addScore(image);
+    this.removeEventListener("click", handler)
+    textLabel = evaluateWin("scissors", compInput); 
+    changeWinnerText(textLabel);
 }); 
